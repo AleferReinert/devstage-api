1b6451c4f609da659cf0bbbb244e33e401e101d2
Alefer Reinert <aleferreinert@gmail.com>
2025-03-08 15:42:38 -0300

    feat: verify if email already exists on register

--- src/functions/subscribeToEvent.ts
+++ src/functions/subscribeToEvent.ts
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm'
 import { db } from '../drizzle/client'
 import { subscriptions } from '../drizzle/schema/subscriptions'
 
@@ -9,6 +10,14 @@ interface SubscribeToEventParams {
 export async function subscribeToEvent({ name, email }: SubscribeToEventParams) {
   const result = await db.insert(subscriptions).values({ name, email }).returning()
   const subscriber = result[0]
+  const subscribers = await db.select().from(subscriptions).where(eq(subscriptions.email, email))
+
+  // If the subscriber already exists, return the id
+  if (subscribers.length > 0) {
+    return {
+      subscriberId: subscribers[0].id,
+    }
+  }
 
   return {
     subscriberId: subscriber.id,
